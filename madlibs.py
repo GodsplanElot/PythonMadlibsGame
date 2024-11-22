5e5d657797c0c73f6ad83e93330b0a61607c8383
GodsplanELot <elotgodsplan2@gmail.com>
2024-11-22 09:13:56 +0100

    created the file and stated out the different ways to concatenate strings in python

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+# string concatenation (basically means how to put strings together)
+
+# Few Ways on how to put string together____________>
+
+#print("Subscribe to" + variable) # using the plus sign
+#print("subscribe to {}".format(variable)) # using the curly brackets with .format(varible) method which will put the variable where the curly brackest are located
+#print(f"subscribe to {variable}") # using the f string, putting the letter f in front of the string and using the curly brackets and putting the variable inbetween
+
+
